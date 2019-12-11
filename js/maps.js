6464f6d16aed9e53bef2dd91d25455a92357c451
cthoms1993@gmail.com <cthoms1993@gmail.com>
2019-12-11 14:09:09 +0000

    info table and info js start

--- js/maps.js
+++ js/maps.js
@@ -1,9 +1,47 @@
+var map, places, infoWindow;
+var markers = [];
+var autocomplete;
+var countryRestrict = {'country': 'uk'};
+var marker_path = 'https://developers.google.com/maps/documentation/javascript/images/marker_red';
+var hostnameRegexp = new RegExp('^https?://.+?/');
+
+var countries = {
+    'uk': {
+        center: {lat: 54.8, lng: -4.6},
+        Zoom: 5
+    },
+    'fr': {
+        center: {lat: 46.2, lng: 2.2},
+        zoom: 5
+    },
+    'es': {
+        center: {lat: 40.5, lng: -3.7},
+        zoom: 5
+    },
+    'de': {
+        center: {lat: 51.2, lng: 10.4},
+        zoom: 5
+    }
+
+};
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 5,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 54.8,
+            lng: -4.6
         }
     });
-}
+ }
+
+ infoWindow = new google.maps.InfoWindow({
+     content: document.getElementById('info-content')
+ });
+
+
+
+
+
+
+
