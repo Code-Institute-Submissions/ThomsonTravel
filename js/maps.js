85c34af0d3f07691f2439e59c62aa36dc5b79770
cthoms1993@gmail.com <cthoms1993@gmail.com>
2019-12-12 21:27:31 +0000

    js full - moved to script tag

--- js/maps.js
+++ js/maps.js
@@ -27,13 +27,13 @@ var countries = {
 
 function initMap(listener) {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 5,
-        center: {
-            lat: 54.8,
-            lng: -4.6
-        }
+        zoom: countries['uk'].zoom,
+        center: countries['uk'].center,
+        mapTypeControl: false,
+        panControl: false,
+        zoomControl: false,
+        streetViewControl: false
     });
-
 
     infoWindow = new google.maps.InfoWindow({
         content: document.getElementById('info-content')
@@ -159,3 +159,65 @@ function clearResults() {
         results.removeChild(results.childNodes[0]);
     }
 }
+
+// Get the place details for a hotel. Show the information in an info window,
+// anchored on the marker for the hotel that the user selected.
+function showInfoWindow() {
+    var marker = this;
+    places.getDetails({placeId: marker.placeResult.place_id},
+        function(place, status) {
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                return;
+            }
+            infoWindow.open(map, marker);
+            buildIWContent(place);
+        });
+}
+
+// Load the place information into the HTML elements used by the info window.
+function buildIWContent(place) {
+    document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
+        'src="' + place.icon + '" alt=""/>';
+    document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
+        '">' + place.name + '</a></b>';
+    document.getElementById('iw-address').textContent = place.vicinity;
+
+    if (place.formatted_phone_number) {
+        document.getElementById('iw-phone-row').style.display = '';
+        document.getElementById('iw-phone').textContent =
+            place.formatted_phone_number;
+    } else {
+        document.getElementById('iw-phone-row').style.display = 'none';
+    }
+
+    if (place.rating) {
+        var ratingHtml = '';
+        for (var i = 0; i < 5; i++) {
+            if (place.rating < (i + 0.5)) {
+                ratingHtml += '&#10025;';
+            } else {
+                ratingHtml += '&#10029;';
+            }
+            document.getElementById('iw-rating-row').style.display = '';
+            document.getElementById('iw-rating').innerHTML = ratingHtml;
+        }
+    } else {
+        document.getElementById('iw-rating-row').style.display = 'none';
+    }
+
+    // The regexp isolates the first part of the URL (domain plus subdomain)
+    // to give a short URL for displaying in the info window.
+    if (place.website) {
+        var fullUrl = place.website;
+        var website = hostnameRegexp.exec(place.website);
+        if (website === null) {
+            website = 'http://' + place.website + '/';
+            fullUrl = website;
+        }
+        document.getElementById('iw-website-row').style.display = '';
+        document.getElementById('iw-website').textContent = website;
+    } else {
+        document.getElementById('iw-website-row').style.display = 'none';
+    }
+}
+
